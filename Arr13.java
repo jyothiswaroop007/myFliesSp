3bb7a99f4bae5bd903b732c9b1e6fe506b97ab4c
jyothiswaroop <65452590+jyothiswaroop007@users.noreply.github.com>
2020-07-10 15:05:35 +0530

    Update Arr13.java

--- Arr13.java
+++ Arr13.java
@@ -1,25 +1,29 @@
 
-public class Arr13{
+public class Demo{
 		
 		public static void main(String args[]) {
-			int [][] arr=new int[2][2];
-			int [][] rev=new int[2][2];
-			int k=0,l=1,p=1;
-			for(int i=0;i<2;i++) {
-				for(int j=0;j<2;j++) {
-					arr[i][j]=Integer.parseInt(args[k++]);
-					rev[l-i][l-j]=arr[i][j];
+			if (args.length!=4) {
+				System.out.println("Please Give 4 commandline arguments");
+			}
+			else {
+				int [][] arr=new int[2][2];
+				int [][] rev=new int[2][2];
+				int k=0,l=1,p=1;
+				for(int i=0;i<2;i++) {
+					for(int j=0;j<2;j++) {
+						arr[i][j]=Integer.parseInt(args[k++]);
+						rev[l-i][l-j]=arr[i][j];
+					}
+				}
+				System.out.println("The given array is:");
+				for(int[] i :arr) {
+					System.out.println(i[0]+" "+i[1]);
+				}
+				System.out.println("The reverse of array is:");
+				for(int[] i :rev) {
+					System.out.println(i[0]+" "+i[1]);
 				}
 			}
-			System.out.println("The given array is:");
-			for(int[] i :arr) {
-				System.out.println(i[0]+" "+i[1]);
-			}
-			System.out.println("The reverse of array is:");
-			for(int[] i :rev) {
-				System.out.println(i[0]+" "+i[1]);
-			}
-			
 	}
 		
 }
